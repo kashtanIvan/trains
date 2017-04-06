c88c7a7cd9b7dae5d4a88b6494946712d51b8510
Kashtan <kashtanivan@yandex.ru>
2017-04-06 12:58:22 +0300

    add test data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,43 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $train = [
+            'name' => 'Train 1',
+            'schedule' => 'пн-пт',
+        ];
+
+        $route = [
+
+                'station_name' => 'Станция 1',
+                'order' => 0,
+                'id_train' => 1,
+        ];
+
+        $train = App\Train::create($train);
+        $route = App\Route::create($route);
+
+        $route = [
+
+            'station_name' => 'Станция 2',
+            'order' => 1,
+            'id_train' => 1,
+
+        ];
+        $route = App\Route::create($route);
+
+        $route =[
+            'station_name' => 'Станция 3',
+            'order' => 2,
+            'id_train' => 1,
+
+        ];
+        $route = App\Route::create($route);
+
+        $route = [
+            'station_name' => 'Станция 4',
+            'order' => 3,
+            'id_train' => 1,
+        ];
+        $route = App\Route::create($route);
     }
 }
