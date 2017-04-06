5e0165d65313113c7a879af7c860a22c6581eddc
Kashtan <kashtanivan@yandex.ru>
2017-04-06 12:57:43 +0300

    add migration

---
+++ database/migrations/2017_04_06_073824_create_routes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('routes', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('id_train', 255)->index();
+            $table->string('station_name', 255)->default('no Station');
+            $table->integer('order')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('routes');
+
+    }
+}
